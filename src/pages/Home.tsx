2cb8fee27ac14f826c07249802465bd552646664
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-08-29 17:21:46 -0500

    🚀 begin: Start Contacts App in Ionic with Tailwind CSS

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen></IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
