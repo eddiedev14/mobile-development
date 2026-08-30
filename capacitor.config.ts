2f382177858f42bc5cb56bc89bddf966cc998ffc
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-08-29 19:32:45 -0500

    ✨ feat: Add android folder and test Ionic App with Android Studio

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'temp-app',
-  webDir: 'dist'
+  appId: "io.ionic.starter",
+  appName: "Contacts App",
+  webDir: "dist",
 };
 
 export default config;
